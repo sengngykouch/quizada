dc998e6ba40d71372b7dfbee5f817cd324dd2d9f
sengngykouch <kouchsengngy@gmail.com>
2024-01-13 17:56:36 -0500

    Updated App to use multipleChoiceViewer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,33 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { JsonReaderComponent } from './json-reader/json-reader.component';
+import { MultipleChoiceViewerComponent } from './multiple-choice-viewer/multiple-choice-viewer.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        CommonModule, RouterOutlet, MatToolbarModule, JsonReaderComponent, MultipleChoiceViewerComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Quizada';
+
+    private _json: string = '';
+
+    public readonly title = 'Quizada';
+
+    public get json() {
+        return this._json;
+    }
+
+    constructor() { }
+
+    protected handleJsonChanged(json: string) {
+        this._json = json;
+    }
 }
